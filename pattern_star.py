a2b4d2e7306e800eaf15afeb06d062f31fb25869
sriram <59157452+sriram-24@users.noreply.github.com>
2020-01-29 12:11:03 +0530

    Add files via upload

---
+++ pattern_star.py
@@ -0,0 +1,15 @@
+
+n=int(input("enter the string"))
+k=2*n-2
+for i in range(0,n):
+    for j in range(0,k):
+        print(end=" ")
+    k=k-1
+    for j in range(0,i+1):
+        print("*",end=" ")
+    print("\r")
+   
+
+      
+        
+      
